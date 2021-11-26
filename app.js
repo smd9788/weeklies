549e0991a3eb1fd1e9e00f253e9b7cb369a8b5f6
Stephen DiCicco <smd09876@gmail.com>
2021-11-26 18:53:37 -0500

    Initial app.js - create inital express shell and server connection - add 'dev' script to package.json

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+const port = process.env.PORT;
+
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello World' })
+})
+
+app.listen(port, () => {
+    console.log(`listening at http://localhost:${port}`)
+});
